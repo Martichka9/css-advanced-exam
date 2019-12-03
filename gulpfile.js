32f0974d5e883e41506a350008712284b38b2712
Martichka9 <Martichka9@users.noreply.github.com>
2019-12-03 16:19:57 +0200

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -52,6 +52,6 @@ gulp.task("watch", function() {
 
 gulp.task("default", gulp.series('jekyll-dev', 'sass', 'watch'));
 
-// gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
-// 	return cp.spawn('git status && git commit -am "Update" && git pull && git push', { stdio: "inherit", shell: true });
-// }));
+gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
+	return cp.spawn('git status && git commit -am "Update" && git pull && git push', { stdio: "inherit", shell: true });
+}));
