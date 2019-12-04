0b41c27a9bd5f9343809d97830f8205698aad14f
Martichka9 <Martichka9@users.noreply.github.com>
2019-12-04 14:26:05 +0200

    Add record layout, records styles, styles updates

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,8 @@ gulp.task("watch", function() {
 			"./*.html",
 			"./*.yml",
 			"./_includes/*.html",
-			"./_layouts/*.html"
+			"./_layouts/*.html",
+			"./_data/*.json"
 		]
 	).on('change', gulp.series('jekyll-dev', 'sass') );
 
@@ -53,5 +54,5 @@ gulp.task("watch", function() {
 gulp.task("default", gulp.series('jekyll-dev', 'sass', 'watch'));
 
 gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
-	return cp.spawn('git status && git commit -am "Update" && git pull && git push', { stdio: "inherit", shell: true });
+	return cp.spawn('git status && git commit -am "Add record layout, records styles, styles updates" && git pull && git push', { stdio: "inherit", shell: true });
 }));
