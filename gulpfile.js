f93877dad13d55a8ef65d574e3a9cefd9cfb23d5
Martichka9 <Martichka9@users.noreply.github.com>
2019-12-05 15:26:52 +0200

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,25 @@ let gulp = require("gulp"),
 	cp = require("child_process");
 
 gulp.task("sass", function() {
-	return gulp.src( '_scss/**/*.scss' )
+	return gulp.src( '_scss/**/*.scss')
 		.pipe( sass().on('error', sass.logError) )
 		.pipe( autoprefixer() )
 		.pipe( csso() )
 		.pipe( gulp.dest( './docs/css/' ) )
 		.pipe( browserSync.stream({ match: '**/*.css' }) )
 	;
+});
+
+gulp.task('copy', function() {
+    return gulp.src([
+		'_scss/fa/css/*.css',
+		'_scss/fa/fa/webfonts/*.eot',
+		'_scss/fa/webfonts/*.svg',
+		'_scss/fa/webfonts/*.ttf',
+		'_scss/fa/webfonts/*.woff',
+		'_scss/fa/webfonts/*.woff2'
+	], {base:'_scss/'})
+        .pipe(gulp.dest('./docs/css/'));
 });
 
 // Jekyll
@@ -45,14 +57,14 @@ gulp.task("watch", function() {
 			"./_layouts/*.html",
 			"./_data/*.json"
 		]
-	).on('change', gulp.series('jekyll-dev', 'sass') );
+	).on('change', gulp.series('jekyll-dev', 'sass', 'copy') );
 
 	gulp.watch( 'docs/**/*.html' ).on('change', browserSync.reload );
 	gulp.watch( 'docs/**/*.js' ).on('change', browserSync.reload );
 });
 
-gulp.task("default", gulp.series('jekyll-dev', 'sass', 'watch'));
+gulp.task("default", gulp.series('jekyll-dev', 'sass', 'copy', 'watch'));
 
-gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
+gulp.task("deploy", gulp.series('jekyll', 'sass', 'copy' , function() {
 	return cp.spawn('git status && git commit -am "Update" && git pull && git push', { stdio: "inherit", shell: true });
 }));
